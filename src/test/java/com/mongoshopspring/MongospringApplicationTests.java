82f092dd602422e88feabc2710b85db2aa4bd5c1
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-05 17:54:56 -0300

    first commit

---
+++ src/test/java/com/mongoshopspring/MongospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

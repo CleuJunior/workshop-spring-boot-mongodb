5e04141f92fdec3b5c5bc972d101cf09b337e225
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-07 23:19:22 -0300

    Insertion User with POST method

--- src/main/java/com/mongoshopspring/resources/UserResource.java
+++ src/main/java/com/mongoshopspring/resources/UserResource.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -42,5 +46,19 @@ public class UserResource {
 
         return ResponseEntity.ok()
                 .body(new UserDTO(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insertNewUser(@RequestBody UserDTO userDTO) {
+        User userObj = this.userService.fromDTO(userDTO);
+        userObj = this.userService.insertUser(userObj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userObj.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri)
+                .build();
     }
 }
--- src/main/java/com/mongoshopspring/services/UserService.java
+++ src/main/java/com/mongoshopspring/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mongoshopspring.services;
 
 import com.mongoshopspring.domain.User;
+import com.mongoshopspring.dto.UserDTO;
 import com.mongoshopspring.repositories.IUserRepository;
 import com.mongoshopspring.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,7 @@ public class UserService {
     IUserRepository userRepository;
 
     @Autowired
-    public UserService(IUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public UserService(IUserRepository userRepository) { this.userRepository = userRepository; }
 
     public List<User> findAll() { return this.userRepository.findAll(); }
 
@@ -25,4 +24,8 @@ public class UserService {
             orElseThrow(() -> new ObjectNotFoundException("User ID not Found"));
 
     }
+
+    public User insertUser(User userObj) { return this.userRepository.insert(userObj); }
+
+    public User fromDTO(UserDTO dtoObj) { return new User(dtoObj.getId(), dtoObj.getName(), dtoObj.getEmail()); }
 }
